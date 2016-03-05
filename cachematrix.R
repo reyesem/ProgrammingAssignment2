19b0c01439787f0717fb71f599f45a63cc748631
reyesem <reyesem@rose-hulman.edu>
2016-03-05 14:29:04 -0500

    Updated cachematrix.R to include my implementation of assignment.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,61 @@
-## Put comments here that give an overall description of what your
-## functions do
+################################################################################
+# Program: cachematrix
+# Description: Set of two functions which computes the inverse of a matrix.
+#              If the inverse has already been computed, then it is retrieved
+#              from cache. Otherwise, the inverse is computed and cached for
+#              future use. Cache is implemented through attributes.
+#
+# Author: Eric Reyes
+# Date: March 2016
+#
+# Notes:
+#  1. Completed in partial fulfillment of R Programming course as part of the
+#      Data Science specialization on Coursera.
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
 
+# function: makeCacheMatrix
+# description: Adds an attribute to a matrix which contains the inverse of the
+#              matrix.
+#
+# parameters:
+#  x           Matrix. The matrix of which to compute the inverse.
+#  ...         Additional parameters to pass to solve().
+#
+# notes:
+#  per the programming instructions, it is assumed that x has an inverse.
+makeCacheMatrix <- function(x = matrix(), ...) {
+  # add attribute to x which contains the inverse.
+  attr(x, "inverse") <- solve(x, ...)
+  
+  # return matrix
+  return(x)
 }
 
 
-## Write a short comment describing this function
-
+# function: cacheSolve
+# description: Implementation of solve() function in base R that computes the
+#              inverse of a matrix. It first checks to see if the inverse has
+#              already been computed and stored as an attribute to x. If not,
+#              then the attribute is added for future use. If so, the inverse
+#              is grabbed instead of recomputed.
+#
+# parameters:
+#  x           Matrix. The matrix of which to compute the inverse.
+#  ...         Additional parameters to pass to solve.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  # if inverse exists, return it.
+  if(!is.null(attr(x, "inverse"))) return(attr(x, "inverse"))
+  
+  # Otherwise, compute inverse and cache it for future use
+  else{
+    # construct inverse
+    new.x <- makeCacheMatrix(x, ...)
+    
+    # cache it for future use
+    eval.parent(substitute(x <- new.x))
+    
+    # return inverse
+    return(attr(new.x, "inverse"))
+  }
 }
